c59113e166656d53169c00a6bedfd4a90851a337
Brian Connelly <brianc@d-69-91-179-157.dhcp4.washington.edu>
2014-06-18 09:48:46 -0700

    Added pushoverr roxygen file

---
+++ R/pushoverr.R
@@ -0,0 +1,24 @@
+#' pushoverr: Send push notifications to iOS and Android devices using Pushover
+#' 
+#' \code{pushoverr} is package for sending push notifications to iOS and Android
+#' devices using \href{https://pushover.net}{Pushover}. Messages can be quickly
+#' sent using the \code{\link{pushover}}, \code{\link{pushover_silent}}, 
+#' \code{\link{pushover_quiet}}, \code{\link{pushover_normal}},
+#' \code{\link{pushover_high}}, and \code{\link{pushover_emergency}} functions.
+#' Behind the scenes, messages are created using \code{\link{PushoverMessage}}
+#' objects, and responses are stored in \code{\link{PushoverResponse}} objects.
+#' 
+#' @docType package
+#' @name pushoverr
+#' @aliases pushoverr-package
+#' @author Brian Connelly
+#' @details For more information, browse the package index by following the link
+#' at the bottom of this help page. For release information, run
+#' \code{news(package='pushoverr')}.
+#' @section Disclaimer: This package and its author are not affiliated with
+#' \href{http://superblock.net}{Superblock, LLC}, creators of Pushover.
+#' @seealso GitHub Repository: \url{https://github.com/briandconnelly/pushoverr}
+#' @seealso Bug Reports and Feature Requests: \url{https://github.com/briandconnelly/pushoverr/issues}
+#' @seealso Pushover: \url{https://pushover.net}
+#' @seealso Pushover API: \url{https://pushover.net/api}
+NULL
