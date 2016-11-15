409a1af53cc3b8e7ad3ae481616931aafbc203bd
Brian Connelly <bdc@bconnelly.net>
2016-11-14 19:46:26 -0800

    Args for update_glance now explicit

--- R/update_glance.R
+++ R/update_glance.R
@@ -1,21 +1,20 @@
 #' Update a Pushover glance data
 #'
 #' Glances allow you to push small pieces of data to a frequently-updated screen
-#' such as a smartwatch or a lock screen.
+#' such as a smartwatch or a lock screen. At least one of the \code{title},
+#' \code{text}, \code{subtext}, \code{count}, or \code{percent} arguments must
+#' be specified.
 #'
 #' @note Glances are currently in beta, and features may change.
+#' @param title (optional) a description of the data being shown, such as "Widgets Sold" (max. 100 characters)
+#' @param text (optional) the main line of data, used on most screens (max. 100 characters)
+#' @param subtext (optional) a second line of data (max. 100 characters)
+#' @param count (optional) integer value shown on smaller screens; useful for simple counts
+#' @param percent (optional) integer percent value (0..100) shown on some screens as a progress bar/circle
 #' @param user user/group key (see \code{\link{set_pushover_user}})
 #' @param app application token (see \code{\link{set_pushover_app}})
 #' @param device (optional) name of the device(s) to send message to. Defaults
 #' to all devices.
-#' @param ... Values for glance data fields. Must provide at least one of:
-#' \itemize{
-#'     \item \code{title}: a description of the data being shown, such as "Widgets Sold" (max. 100 characters)
-#'     \item \code{text}: the main line of data, used on most screens (max. 100 characters)
-#'     \item \code{subtext}: a second line of data (max. 100 characters)
-#'     \item \code{count}: integer value shown on smaller screens; useful for simple counts
-#'     \item \code{percent}: integer percent value (0..100) shown on some screens as a progress bar/circle
-#' }
 #'
 #' @return an invisible list containing the following fields:
 #' \itemize{
@@ -30,46 +29,47 @@
 #' \dontrun{
 #' update_glance(count = 37)
 #' }
-update_glance <- function(user = get_pushover_user(), app = get_pushover_app(),
+update_glance <- function(title = NULL, text = NULL, subtext = NULL,
+                          count = NULL, percent = NULL,
+                          user = get_pushover_user(), app = get_pushover_app(),
                           device = NULL, ...) {
 
+    if(is.null(c(title, text, subtext, count, percent))) {
+        stop("Must provide at least one of the following arguments: title, text, subtext, count, percent", call. = FALSE)
+    }
     assertthat::assert_that(assertthat::is.scalar(user))
     assertthat::assert_that(assertthat::is.scalar(app))
 
     params <- list("token" = app, "user" = user)
+
+    if (!is.null(title)) {
+        assertthat::assert_that(nchar(title) <= 100)
+        params$title <- title
+    }
+    
+    if (!is.null(text)) {
+        assertthat::assert_that(nchar(text) <= 100)
+        params$text <- text
+    }
+    
+    if (!is.null(subtext)) {
+        assertthat::assert_that(nchar(subtext) <= 100)
+        params$subtext <- subtext
+    }
+    
+    if (!is.null(count)) {
+        assertthat::assert_that(assertthat::is.number(count))
+        params$count <- count
+    }
+    
+    if (!is.null(percent)) {
+        assertthat::assert_that(assertthat::is.number(percent) && percent >= 0 && percent <= 100)
+        params$percent <- percent
+    }
+    
     if (!is.null(device)) {
         assertthat::assert_that(assertthat::is.scalar(device))
         params$device <- device
-    }
-
-    other_args <- list(...)
-    glance_fields <- c("title", "text", "subtext", "count", "percent")
-    assertthat::assert_that(any(glance_fields %in% names(other_args)))
-
-    if ("title" %in% names(other_args)) {
-        assertthat::assert_that(nchar(other_args[["title"]]) <= 100)
-        params$title <- other_args[["title"]]
-    }
-
-    if ("text" %in% names(other_args)) {
-        assertthat::assert_that(nchar(other_args[["text"]]) <= 100)
-        params$text <- other_args[["text"]]
-    }
-
-    if ("subtext" %in% names(other_args)) {
-        assertthat::assert_that(nchar(other_args[["subtext"]]) <= 100)
-        params$subtext <- other_args[["subtext"]]
-    }
-
-
-    if ("count" %in% names(other_args)) {
-        assertthat::assert_that(assertthat::is.number(other_args[["count"]]))
-        params$count <- other_args[["count"]]
-    }
-
-    if ("percent" %in% names(other_args)) {
-        assertthat::assert_that(assertthat::is.number(other_args[["percent"]]))
-        params$percent <- other_args[["percent"]]
     }
 
     response <- httr::POST(url = "https://api.pushover.net/1/glances.json",
