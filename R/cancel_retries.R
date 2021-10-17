908bff9e0234db95c3f597f1a7e877853786e3be
Brian Connelly <bdc@bconnelly.net>
2021-10-17 12:27:10 -0700

    Reformat `cancel_retries()` docs, remove deprecated function

--- R/cancel_retries.R
+++ R/cancel_retries.R
@@ -1,17 +1,17 @@
 #' Cancel retries for an emergency priority notification
 #'
-#' \code{cancel_retries} stops Pushover from sending repeat messages for
+#' `cancel_retries()` stops Pushover from sending repeat messages for
 #' un-acknowledged emergency priority notifications.
 #'
 #' @param receipt The receipt from sending an emergency message
-#' @param app application token (see \code{\link{set_pushover_app}})
+#' @param app application token (see [`set_pushover_app()`])
 #'
 #' @return an invisible list containing the following fields:
 #' \itemize{
-#'     \item \code{status}: request status (1 = success)
-#'     \item \code{request}: unique request ID
-#'     \item \code{errors}: a list of error messages (only for unsuccessful requests)
-#'     \item \code{raw}: the raw \code{\link[httr]{response}} object
+#'     \item `status`: request status (1 = success)
+#'     \item `request`: unique request ID
+#'     \item `errors`: a list of error messages (only for unsuccessful requests)
+#'     \item `raw`: the raw [httr::response] object
 #' }
 #' @export
 #'
@@ -34,21 +34,4 @@ cancel_retries <- function(receipt, app = get_pushover_app()) {
     verb = "POST", url = query_url, visible = FALSE,
     body = list("token" = app)
   )
-}
-
-#' @rdname cancel_retries
-#' @description \code{cancel_receipt} is deprecated in favor of \code{cancel_retries}
-#' @param ... Additional arguments (no longer used)
-#' @export
-cancel_receipt <- function(receipt, ...) {
-  message("cancel_receipt() is deprecated. Please use cancel_retries() instead.")
-  assertthat::assert_that(is.valid_receipt(receipt))
-
-  opt_args <- list(...)
-  app <- ifelse("token" %in% names(opt_args),
-    opt_args[["token"]],
-    get_pushover_app()
-  )
-
-  cancel_retries(receipt = receipt, app = app)
 }
